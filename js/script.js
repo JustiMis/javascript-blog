8175ec5aae34dc59755cf8435b1826e3c6375a8d
Justyna Misiuk <bir8@wp.pl>
2020-10-06 15:01:49 +0200

    Done active link

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function titleClickHandler(event){
-  console.log(event);
+    const clickedElement = this;
+  console.log('Link was clicked');
 
   /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -10,6 +11,8 @@ function titleClickHandler(event){
     activeLink.classList.remove('active');
   }
   /* add class 'active' to the clicked link */
+  console.log('clickedElement:' + clickedElement);
+  clickedElement.classList.add('active');
   
   /* remove class 'active' from all articles */
   const activeArticle = document.querySelectorAll('.article active');
