9c3a9d5de24dd2424400c9faff46cb05ccd7334d
Justyna Misiuk <bir8@wp.pl>
2020-10-08 12:48:54 +0200

    Workin articles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 'use strict';
 
+
+
 function titleClickHandler(event){
+    event.preventDefault();
     const clickedElement = this;
   console.log('Link was clicked');
 
@@ -15,16 +18,22 @@ function titleClickHandler(event){
   clickedElement.classList.add('active');
   
   /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.article active');
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link */
+     const articleSelector = clickedElement.getAttribute('href');
+     console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
 
   /* add class 'active' to the correct article */
+      targetArticle.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
