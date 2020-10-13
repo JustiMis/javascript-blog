46628b27360147fdb616b9bebd744a0d9b371ff9
Justyna Misiuk <bir8@wp.pl>
2020-10-13 11:47:57 +0200

    Generated list title

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 'use strict';
-
-
-
+/*wyświetlanie artykułu po kliknieciu  */
 function titleClickHandler(event){
     event.preventDefault();
     const clickedElement = this;
@@ -35,6 +33,59 @@ function titleClickHandler(event){
       targetArticle.classList.add('active');
 
 }
+
+
+/* generowanie listy tytułów */
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+console.log('utworzono funkcje generateTitleLinks')
+
+  /* remove contents of titleList */
+const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+console.log('utworzono stała o nazwie titleList, która wybiera wszystkie titles i wypełnia je pusta zawartościa(usuwamy titleList)')
+
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector); //*'.posts article'
+  console.log('utworzono stała articles, która skanuje po kolei kazdy article i zapisuje go do zmiennej')
+  
+  let html = '';
+
+  for(let post of articles){
+  console.log('w petli dla kazdego post w article') 
+    
+     /* get the article id */
+     const articleId = post.getAttribute('id');
+     console.log('pobieram id z kazdego article')
+    
+     /* find the title element */
+     const titleElement = post.querySelector(optTitleSelector);
+     console.log('znajduje title w kazdym article')
+
+     /* get the title from the title element */
+     const titleValue = titleElement.innerHTML;
+     console.log('pobieram title z wnetrza html') /* w jednej linijce const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+     /* create HTML of the link */
+     const HTML = '<li><a href="#' + articleId + '"><span>' + titleValue + '</span></a></li>';
+     console.log('utworzono stala html która tworzy linijke listy, wartość atrybutu href to stała: ' + articleId + ' z dodanym tytułem: ' + titleValue);
+    
+     /* insert link into html variable */
+     html = html + HTML;
+     console.log('zmienna html, po każdym obrocie petli dodaje nowy link jesli sie pojawił')
+  }
+    /* insert link into titleList */
+  titleList.innerHTML = html;
+    // przed wprowadzeniem zmiennej html: titleList.innerHTML = titleList.innerHTML + HTML
+    // alternatywnie: titleList.insertAdjacentHTML('beforeend',HTML);
+  }
+
+
+generateTitleLinks();
 
 const links = document.querySelectorAll('.titles a');
 
